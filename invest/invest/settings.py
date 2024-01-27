034c3917aa205c326b09427d74615cd74784c0c1
Emmanuel1 <Nnamdiemmanuel01@gmail.com>
2024-01-27 13:41:18 +0100

    settings

--- invest/invest/settings.py
+++ invest/invest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-++_g$)i^oqegbsfp#^d8c+&y5tv88sga697ar!*ceb11no!c72'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rep1-bb1d002fb085.herokuapp.com/']
 
 
 # Application definition
